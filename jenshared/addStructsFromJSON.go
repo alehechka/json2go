9263c23ea3cf01e3b4dbb9a558dcbb738f258bef
Adam Lehechka <42357034+alehechka@users.noreply.github.com>
2022-06-25 11:22:45 -0500

    resolve top-level array generation issue

--- jenshared/addStructsFromJSON.go
+++ jenshared/addStructsFromJSON.go
@@ -25,7 +25,25 @@ func parseInterface(items TypeItemsMap, data interface{}, config *Config) TypeIt
 	case map[string]interface{}:
 		parseMap(items, concreteVal, config.RootName)
 	case []interface{}:
-		parseFirstIndexArray(items, concreteVal, config.RootName)
+		diveTopLevelArray(items, concreteVal, config, "[]")
+	}
+
+	return items
+}
+
+func diveTopLevelArray(items TypeItemsMap, data []interface{}, config *Config, acc string) TypeItemsMap {
+	if len(data) > 0 {
+		switch firstVal := data[0].(type) {
+		case bool, float64, string:
+			items[config.RootName] = TypeItems{{Name: config.RootName, Type: fmt.Sprintf("%s%s", acc, inferDataType(firstVal))}}
+		case map[string]interface{}:
+			arrTitle := fmt.Sprintf("%sArray", config.RootName)
+			items[arrTitle] = TypeItems{{Name: arrTitle, Type: fmt.Sprintf("%s%s", acc, config.RootName)}}
+			parseMap(items, firstVal, config.RootName)
+
+		case []interface{}:
+			diveTopLevelArray(items, firstVal, config, fmt.Sprintf("%s[]", acc))
+		}
 	}
 
 	return items
@@ -40,8 +58,6 @@ func parseMap(items TypeItemsMap, data map[string]interface{}, parent string) Ty
 			items[parent] = append(items[parent], TypeItem{Name: key, Type: title})
 			parseMap(items, concreteVal, title)
 		case []interface{}:
-			arrTitle := fmt.Sprintf("%sArray", parent)
-			items[arrTitle] = TypeItems{{Name: arrTitle, Type: fmt.Sprintf("[]%s", parent)}}
 			items[parent] = append(items[parent], TypeItem{Name: key, Type: fmt.Sprintf("[]%s", title)})
 			parseFirstIndexArray(items, concreteVal, title)
 		default:
@@ -53,27 +69,27 @@ func parseMap(items TypeItemsMap, data map[string]interface{}, parent string) Ty
 
 func parseFirstIndexArray(items TypeItemsMap, array []interface{}, parent string) TypeItemsMap {
 	if len(array) > 0 {
-		val := array[0]
-		switch concreteVal := val.(type) {
+		switch concreteVal := array[0].(type) {
 		case map[string]interface{}:
 			parseMap(items, concreteVal, parent)
 		case []interface{}:
-			delete(items, parent)
+		InterfaceArrayOuter:
 			for key, itemArray := range items {
 				for index, item := range itemArray {
 					if item.Title() == parent {
 						items[key][index].Type = fmt.Sprintf("[]%s", item.Type)
 						parseFirstIndexArray(items, concreteVal, parent)
-						break
+						break InterfaceArrayOuter
 					}
 				}
 			}
 		default:
-			delete(items, parent)
+		DefaultOuter:
 			for key, itemArray := range items {
 				for index, item := range itemArray {
 					if item.Title() == parent && strings.HasSuffix(item.Type, parent) {
 						items[key][index].Type = fmt.Sprintf("%s%s", strings.TrimSuffix(item.Type, parent), inferDataType(concreteVal))
+						break DefaultOuter
 					}
 				}
 			}
